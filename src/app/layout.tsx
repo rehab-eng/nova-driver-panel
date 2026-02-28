cd00374b8a86f7f4804257f2b28e17754345f2a7
Mohamed <hsjshdvehhs@gmail.com>
2026-02-28 03:05:02 +0200

    chore: set metadata base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nova-driver-panel.pages.dev"),
   title: {
     default: "Nova Max | ???? ???? ???????",
     template: "%s | Nova Max",
